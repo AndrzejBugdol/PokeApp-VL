b433cbac1f05917f9726ead47f60b5eff37da7ba
Bugdol <andrzej.bugdol@cytiva.com>
2022-05-06 10:45:10 +0200

    Import PokemonType fix

--- src/Components/PokemonList/TypedPokeList.tsx
+++ src/Components/PokemonList/TypedPokeList.tsx
@@ -1,5 +1,4 @@
 import React, {useState,useEffect} from 'react'
-import { PokemonType } from '../../Utils/ColorsUtils'
 import { SearchBar } from './SearchBar'
 import { Card } from '../Card/Card'
 import {Pokemon} from "../../model"
